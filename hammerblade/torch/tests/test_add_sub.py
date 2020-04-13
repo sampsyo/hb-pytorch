8ccdd6dbce1809ebea0137bc788d3feeda4780ee
AngelaZou2000 <az292@cornell.edu>
2020-04-13 09:49:40 -0400

    debu

--- hammerblade/torch/tests/test_add_sub.py
+++ hammerblade/torch/tests/test_add_sub.py
@@ -25,11 +25,16 @@ def _test_add(x1, x2):
     y_h = h1 + h2
     assert y_h.device == torch.device("hammerblade")
     assert torch.allclose(y_c, y_h.cpu())
-    # # inplace
-    # x1.add_(x2)
-    # h1.add_(h2)
-    # assert h1.device == torch.device("hammerblade")
-    # assert torch.allclose(x1, h1.cpu())
+    # inplace
+    x1.add_(x2)
+    h1.add_(h2)
+    assert h1.device == torch.device("hammerblade")
+    assert torch.allclose(x1, h1.cpu())
+
+def test_add_4():
+    x1 = torch.rand(4)
+    x2 = torch.rand(4)
+    _test_add(x1, x2)
 
 def test_add_64():
     x1 = torch.rand(64)

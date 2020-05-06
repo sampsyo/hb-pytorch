97b0cd0dfd3f6b579f3147186d52ef7e235e9288
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-05-06 14:02:50 -0700

    Flow for runnign natively

--- setup_native_build_env.sh
+++ setup_native_build_env.sh
@@ -1,13 +1,12 @@
 # setup brg sdh pytorch building env
-echo "  Setting up PyTorch building environment ... for native evaluation!"
+echo "  Setting up PyTorch building environment ... "
 echo "  Make sure you enabled devtoolset-8!"
+echo "  Make sure correct Python environemnt is set!"
 echo ""
 
 # setup pytorch building options
+export REL_WITH_DEB_INFO=1
 export BUILD_TEST=0
-export USE_HB=0
-export BLAS=OpenBLAS
-export OpenBLAS_HOME=<YOUR_OpenBLAS_HOME_PATH>
 export USE_MKL=0
 export USE_MKLDNN=0
 export USE_CUDA=0
@@ -15,12 +14,41 @@ export USE_CUDNN=0
 export USE_FBGEMM=0
 export USE_NNPACK=0
 export USE_QNNPACK=0
+export USE_DISTRIBUTED=0
+export USE_OPENMP=0
+export ATEN_THREADING=NATIVE
+export OMP_NUM_THREADS=1
 
 # Use gold if it's available for faster linking.
 if which gold >/dev/null 2>&1 ; then
     export CFLAGS='-fuse-ld=gold'
 fi
 
-# make sure real bsg_manycore_cuda library is not exported
-unset BSG_MANYCORE_DIR
-unset HB_KERNEL_DIR
+# get current directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+echo "  hb-pytorch lives in $DIR"
+
+export BRG_BSG_BLADERUNNER_DIR="$DIR/../bsg_bladerunner"
+
+# setup cudalite runtime and pytorch kernel binary paths
+if [ -z "$BRG_BSG_BLADERUNNER_DIR" ]
+then
+  export BSG_MANYCORE_DIR="<path-to-your-cudalite-cosim-runtime>"
+else
+  export BSG_MANYCORE_DIR=/usr/lib64
+fi
+
+export HB_KERNEL_DIR=$DIR/hammerblade/torch
+
+echo "  \$BSG_MANYCORE_DIR is set to $BSG_MANYCORE_DIR"
+echo "  \$HB_KERNEL_DIR is set to $HB_KERNEL_DIR"
+echo ""
+echo "  Done!"
+echo ""

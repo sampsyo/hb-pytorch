5b0f772ec4e5ad5679e2486c302787c8f91f14b8
lc873 <lc873@cornell.edu>
2020-06-11 16:07:09 -0400

    [sort] fix merge sort

--- hammerblade/torch/kernel/kernel_vsort.cpp
+++ hammerblade/torch/kernel/kernel_vsort.cpp
@@ -8,56 +8,56 @@
 
 // Helper function to perform Merge Sort
 static void merge(
-	HBTensor<float>* tmp_p, 
-	HBTensor<float>* vec0,
-	int32_t begin0,
-	int32_t end0,
-	HBTensor<float>* vec1,
-	int32_t begin1,
-	int32_t end1) {
-	int32_t size = (end0 - begin0) + (end1 - begin1);
-	int32_t idx0 = begin0;
-	int32_t idx1 = begin1;
-	for (size_t i = 0; i < size; i++) {
-		if ( idx0 == end0 ) {
-			(*tmp_p)(i) = (*vec1)(idx1);
-			idx1 ++;
-		}
-		else if ( idx1 == end1 ) {
-			(*tmp_p)(i) = (*vec0)(idx0);
-			idx0 ++;
-		}
-		else if ((*vec0)(idx0) <= (*vec1)(idx1)) {
-			(*tmp_p)(i)= (*vec0)(idx0);
-			idx0 ++;
-		}
-		else {
-			(*tmp_p)(i) = (*vec1)(idx1);
-			idx1 ++;
-		}
-	}
+  HBTensor<float>* tmp_p,
+  HBTensor<float>* vec0,
+  int32_t begin0,
+  int32_t end0,
+  HBTensor<float>* vec1,
+  int32_t begin1,
+  int32_t end1) {
+  int32_t size = (end0 - begin0) + (end1 - begin1);
+  int32_t idx0 = begin0;
+  int32_t idx1 = begin1;
+  for (size_t i = 0; i < size; i++) {
+    if ( idx0 == end0 ) {
+      (*tmp_p)(i) = (*vec1)(idx1);
+      idx1 ++;
+    }
+    else if ( idx1 == end1 ) {
+      (*tmp_p)(i) = (*vec0)(idx0);
+      idx0 ++;
+    }
+    else if ((*vec0)(idx0) <= (*vec1)(idx1)) {
+      (*tmp_p)(i)= (*vec0)(idx0);
+      idx0 ++;
+    }
+    else {
+      (*tmp_p)(i) = (*vec1)(idx1);
+      idx1 ++;
+    }
+  }
 }
 
 // Recursive merge helper function
 static void msort_op_h(HBTensor<float>* result_p,
-	HBTensor<float>* tmp_p,
-	HBTensor<float>* vec,
-	int32_t begin,
-	int32_t end) {
-	int32_t size = end - begin;
-	if (size <= 1) {
-		return;
-	}
-	int32_t mid =int32_t( (begin + end) / 2 );
-	std::cout<<"mid variable is "<<mid;
-	msort_op_h(result_p, tmp_p, vec, begin, mid);
-	msort_op_h(result_p, tmp_p, vec, mid, end);
-	merge(tmp_p, vec, begin, mid, vec, mid, end);
-	int32_t x = 0;
-	for (size_t i = begin; i < end; i++) {
-		(*result_p)(i) = (*tmp_p)(x);
-		x++;
-	}
+  HBTensor<float>* tmp_p,
+  HBTensor<float>* vec,
+  int32_t begin,
+  int32_t end) {
+  int32_t size = end - begin;
+  if (size <= 1) {
+    (*result_p)(begin) = (*vec)(begin);
+    return;
+  }
+  int32_t mid =int32_t( (begin + end) / 2 );
+  msort_op_h(result_p, tmp_p, vec, begin, mid);
+  msort_op_h(result_p, tmp_p, vec, mid, end);
+  merge(tmp_p, result_p, begin, mid, result_p, mid, end);
+  int32_t x = 0;
+  for (size_t i = begin; i < end; i++) {
+    (*result_p)(i) = (*tmp_p)(x);
+    x++;
+  }
 }
 
 extern "C" {
@@ -76,9 +76,9 @@ extern "C" {
     
     // Use a single tile only
     if(__bsg_id == 0) {
-		msort_op_h(&result, &tmp, &self, 0, self.numel());
-	}
-	//quicksort(&result, 0, self.numel()-1);
+      msort_op_h(&result, &tmp, &self, 0, self.numel());
+    }
+  //quicksort(&result, 0, self.numel()-1);
     //   End profiling
     bsg_cuda_print_stat_kernel_end();
 

a92c0ac5815e366fbaf0a195cbecaf25cdad4d17
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-06-27 12:50:32 -0700

    restrict in binary hb_tiled_foreach

--- hammerblade/torch/kernel/hb_tiled_for.hpp
+++ hammerblade/torch/kernel/hb_tiled_for.hpp
@@ -307,15 +307,13 @@ inline void hb_tiled_foreach(HBTensor<scalar_t> res,
 //==========================================================
 
 template<typename scalar_t, typename F>
-inline void hb_tiled_foreach(HBTensor<scalar_t> res,
-                               HBTensor<scalar_t> input,
-                               HBTensor<scalar_t> other,
-                               F functor) {
-  __remote char* data[3];
-  data[0] = res.data_ptr();
-  data[1] = input.data_ptr();
-  data[2] = other.data_ptr();
-
+__attribute__((noinline)) void hb_tiled_foreach_impl(HBTensor<scalar_t> res,
+                                  HBTensor<scalar_t> input,
+                                  HBTensor<scalar_t> other,
+                                  __remote char* __restrict resptr,
+                                  __remote char* __restrict inputptr,
+                                  __remote char* __restrict otherptr,
+                                  F functor) {
   // is_trivial_1d
   if(res.ndim() == 1) {
 
@@ -334,17 +332,17 @@ inline void hb_tiled_foreach(HBTensor<scalar_t> res,
     size_t start = len_per_tile * __bsg_id;
     size_t end = start + len_per_tile;
     end = (end > res.numel())  ? res.numel() : end;
-    data[0] += strides[0] * start;
-    data[1] += strides[1] * start;
-    data[2] += strides[2] * start;
+    resptr += strides[0] * start;
+    inputptr += strides[1] * start;
+    otherptr += strides[2] * start;
     for (size_t idx = start; idx < end; idx++) {
-      __remote scalar_t* res_dp = (__remote scalar_t*)(data[0]);
-      __remote scalar_t* input_dp = (__remote scalar_t*)(data[1]);
-      __remote scalar_t* other_dp = (__remote scalar_t*)(data[2]);
+      __remote scalar_t* res_dp = (__remote scalar_t*)(resptr);
+      __remote scalar_t* input_dp = (__remote scalar_t*)(inputptr);
+      __remote scalar_t* other_dp = (__remote scalar_t*)(otherptr);
       *res_dp = functor(*input_dp, *other_dp);
-      data[0] += strides[0];
-      data[1] += strides[1];
-      data[2] += strides[2];
+      resptr += strides[0];
+      inputptr += strides[1];
+      otherptr += strides[2];
     }
   } else {
     //-----------------------------
@@ -355,12 +353,24 @@ inline void hb_tiled_foreach(HBTensor<scalar_t> res,
     size_t end = start + len_per_tile;
     end = (end > res.numel())  ? res.numel() : end;
     for (size_t idx = start; idx < end; idx++) {
-      __remote scalar_t* res_dp = (__remote scalar_t*)(data[0] + offset_calc(idx, res));
-      __remote scalar_t* input_dp = (__remote scalar_t*)(data[1] + offset_calc(idx, input));
-      __remote scalar_t* other_dp = (__remote scalar_t*)(data[2] + offset_calc(idx, other));
+      __remote scalar_t* res_dp = (__remote scalar_t*)(resptr + offset_calc(idx, res));
+      __remote scalar_t* input_dp = (__remote scalar_t*)(inputptr + offset_calc(idx, input));
+      __remote scalar_t* other_dp = (__remote scalar_t*)(otherptr + offset_calc(idx, other));
       *res_dp = functor(*input_dp, *other_dp);
     }
   }
+}
+
+template<typename scalar_t, typename F>
+__attribute__((noinline)) void hb_tiled_foreach(HBTensor<scalar_t> res,
+                               HBTensor<scalar_t> input,
+                               HBTensor<scalar_t> other,
+                               F functor) {
+  hb_tiled_foreach_impl(res, input, other,
+                        res.data_ptr(),
+                        input.data_ptr(), 
+                        other.data_ptr(),
+                        functor);
 }
 
 // =========================================================

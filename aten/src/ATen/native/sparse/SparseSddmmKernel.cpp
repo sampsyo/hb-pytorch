bf02589a14a29d5a2a4f298967b65e259c7f2054
Andrew Martin Pareles <amp342@gorgonzola.cs.cornell.edu>
2020-07-29 18:51:07 -0400

    update sddmm, comment out dstmm for now

--- aten/src/ATen/native/sparse/SparseSddmmKernel.cpp
+++ aten/src/ATen/native/sparse/SparseSddmmKernel.cpp
@@ -8,38 +8,34 @@ namespace native {
 
 using namespace at::sparse;
 
-template <typename scalar_t>
-void inline sddmm_kernel(
-    const Tensor& a_sparse,
-    const Tensor& b_dense,
-    const Tensor& c_dense,
-    Tensor& out_tensor) {
 
-    auto a_indices = a_sparse._indices();
+Tensor sddmm_cpu(
+  const SparseTensor& a_sparse_tensor,
+  const Tensor& b_dense_tensor,
+  const Tensor& c_dense_tensor) {
+  Tensor out_tensor = at::zeros(a_sparse_tensor.sizes(), {at::requires_grad().device(at::kCPU).dtype(at::kFloat)});
+  AT_DISPATCH_ALL_TYPES(b_dense_tensor.scalar_type(), "sddmm_cpu", [&] {
+
+    auto a_indices = a_sparse_tensor._indices().accessor<int64_t, 2>();
+    auto b_dense = b_dense_tensor.accessor<scalar_t, 2>();
+    auto c_dense = c_dense_tensor.accessor<scalar_t, 2>();
+    auto out = out_tensor.accessor<scalar_t, 2>();
 
     int dot_len = b_dense.size(1);
-    for (int k = 0; k < a_sparse._nnz(); k++) {
-      int ai = a_indices(0, k);//0
-      int aj = a_indices(1, k);  //1
+    for (int k = 0; k < a_sparse_tensor._nnz(); k++) {
+      int ai = a_indices[0][k]; //0
+      int aj = a_indices[1][k];  //1
 
       float dot_total = 0;
       for (int i = 0; i < dot_len; i++)
-        dot_total += b_dense(ai,i) * c_dense(i,aj);
+        dot_total += b_dense[ai][i] * c_dense[i][aj];
         
-      out_tensor(ai,aj) = dot_total;
+      out[ai][aj] = dot_total;
     }
+  
+  });
+  return out_tensor;
 }
-
-Tensor sddmm_cpu(
-  const SparseTensor& a_sparse,
-  const Tensor& b_dense,
-  const Tensor& c_dense) {
-  Tensor out_dense = at::zeros(a_sparse.sizes(), {at::requires_grad().device(at::kCPU).dtype(at::kFloat)});
-
-  sddmm_kernel<float>(a_sparse, b_dense, c_dense, out_dense);
-  return out_dense;
-}
-
 
 } // namespace native
 } // namespace at
